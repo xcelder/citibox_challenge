6a79cb9a366661b149b59c707cad93ac867006fb
Sergio Pascual <xcelder@gmail.com>
2021-04-04 20:53:25 +0200

    add retrofit

--- usecase/src/main/java/com/example/usecase/GetEpisodes.kt
+++ usecase/src/main/java/com/example/usecase/GetEpisodes.kt
@@ -3,5 +3,5 @@ package com.example.usecase
 import com.example.domain.repositories.EpisodesRepository
 
 class GetEpisodes(private val episodesRepository: EpisodesRepository) {
-    suspend operator fun invoke(episodeNumbers: List<String>) = episodesRepository.getEpisodes(episodeNumbers)
+    suspend operator fun invoke(episodeNumbers: List<Int>) = episodesRepository.getEpisodes(episodeNumbers)
 }
